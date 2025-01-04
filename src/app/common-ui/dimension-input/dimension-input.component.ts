a56c0cfaf0a1c270ff288b16f35dadab97dcb759
Qyea <kreshess@mail.ru>
2025-01-04 23:55:20 +0300

    Error correction: Removing components from an unsuitable library

--- src/app/common-ui/dimension-input/dimension-input.component.ts
+++ src/app/common-ui/dimension-input/dimension-input.component.ts
@@ -1,16 +1,14 @@
-import { Component, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-dimension-input',
   standalone: true,
-  imports: [InputNumberModule, FormsModule, CommonModule],
+  imports: [FormsModule],
   templateUrl: './dimension-input.component.html',
   styleUrl: './dimension-input.component.scss',
 })
 export class DimensionInputComponent {
-  dementionN: number = 42723;
-  dementionM: number = 42723;
+  title: string = 'hello';
 }
